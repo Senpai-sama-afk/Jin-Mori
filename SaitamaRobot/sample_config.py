f5f5b941ed16b104ecea413a970b715878d67bac
AiyaGiorno <79815973+AiyaGiorno@users.noreply.github.com>
2021-03-01 19:31:25 +0530

    Update sample_config.py

--- SaitamaRobot/sample_config.py
+++ SaitamaRobot/sample_config.py
@@ -19,12 +19,12 @@ class Config(object):
     TOKEN = "1640151803:AAFDTDFCZxvZFafYrgjYYpos6k9BxmojbiA"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "ST4RPL4TINUM"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "Jin Mori Support Group"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001293324525
+        -1001192326978
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001293324525
+        -1001192326978
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
